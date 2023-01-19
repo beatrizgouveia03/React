bfda6ae260b50adcf114991fff13c8b46f39c41a
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-01-19 20:11:09 -0300

    Import changes

--- nlw/web/src/App.tsx
+++ nlw/web/src/App.tsx
@@ -5,10 +5,8 @@ import { Habit } from './components/Habit'
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
       <Habit completed ={3}/>
       <Habit completed={10}/>
-    
     </div>    
   )
 }
