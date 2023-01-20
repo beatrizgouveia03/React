6b35dbfe538cb7cd2910d9bd983fceeb0640e169
Beatriz <beatrizgouveia.gadelha0@gmail.com>
2023-01-20 16:19:18 -0300

    Server Bugs fixed

--- nlw/server/src/server.ts
+++ nlw/server/src/server.ts
@@ -8,8 +8,8 @@ const prisma = new PrismaClient()
 app.register(cors)
 
 app.get('/hello', async () => {
-    const habits = await prisma.habit.findMany()
-
+    const habits = await prisma.habit.findMany({ })
+    
     return habits
 })
 
