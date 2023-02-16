66cbf5a32b033b6fe9b4bcfbb8638a26a1ed86d0
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-02-16 12:28:40 -0300

    finished

--- modal/puro/js/custom.js
+++ modal/puro/js/custom.js
@@ -1,7 +1,15 @@
 function abrirModal(carregarModal) {
     console.log("Carregar modal: " + carregarModal)
+    let modal = document.getElementById(carregarModal);
+
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden'
 }
 
 function fecharModal(fecharModal) {
     console.log("Fechar modal: " + fecharModal)
+    let modal = document.getElementById(fecharModal);
+
+    modal.style.display = 'none';
+    document.body.style = 'auto';
 }
