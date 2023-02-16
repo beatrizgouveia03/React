0e7607d4f50394e35b18cb75275fa1eaaf695fde
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-02-16 18:35:10 -0300

    modal com react funcionando

--- modal/src/App.js
+++ modal/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+
+function abrirModal(carregarModal) {
+  console.log("Abrir");
+  let modal = document.getElementById(carregarModal);
+
+  modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+}
+
+function  fecharModal(fecharModal) {
+  let modal = document.getElementById(fecharModal);
+
+  modal.style.display = 'none';
+  document.body.style.overflow = 'auto';
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Janela Modal</h1>
+
+      <button className="btn-outline-success" onClick={() =>abrirModal('vis-modal')}>Abrir Modal</button>
+
+      <div id="vis-modal" className="modal">
+        <div className="conteudo-modal">
+            <div className="cabecalho-modal">
+                <h1 className="titulo-modal">Visualizar</h1>
+                <span 
+                    className="cabecalho-modal-fechar"
+                    onClick={() => fecharModal('vis-modal')}    
+                >X</span>
+            </div>
+
+            <div className="corpo-modal">
+                teste modal
+            </div>
+            <div className="rodape-modal">
+                <button
+                    className="btn-outline-success"
+                    onClick={() => fecharModal('vis-modal')}
+                >Fechar</button>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
