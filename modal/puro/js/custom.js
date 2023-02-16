44a026a4f785f9bb879b9cf9d1695d5cad74835c
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-02-16 11:49:35 -0300

    Teste modal

---
+++ modal/puro/js/custom.js
@@ -0,0 +1,7 @@
+function abrirModal(carregarModal) {
+    console.log("Carregar modal: " + carregarModal)
+}
+
+function fecharModal(fecharModal) {
+    console.log("Fechar modal: " + fecharModal)
+}
