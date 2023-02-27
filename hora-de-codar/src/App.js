0396164acbead75059d2d773cb4fe258c11e7e4a
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-02-27 11:48:57 -0300

    Aula 7

--- hora-de-codar/src/App.js
+++ hora-de-codar/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import Frase from './components/Frase';
+import List from './components/List';
 import Pessoa from './components/Pessoa';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
+      <HelloWorld /> <br />
       <Pessoa 
         nome="Rodrigo"
         idade="28"
         profissao="Programador"
         foto='https:/via.placeholder.com/150'
       />
+      <Frase /> <br />
       <Frase />
+      <List />
+      
     </div>
   );
 }
