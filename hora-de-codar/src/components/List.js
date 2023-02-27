4a18b230c8ca61a1899d04d59e5a90c1b5e13f7a
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-02-27 11:56:24 -0300

    Aula 8

--- hora-de-codar/src/components/List.js
+++ hora-de-codar/src/components/List.js
@@ -5,9 +5,10 @@ function List() {
         <>
             <h1>Minha lista</h1>
             <ul>
-                <Item marca="Ferrari" />
-                <Item marca="Fiat" />
-                <Item marca="Renault" />
+                <Item marca="Ferrari" lancamento={1985}/>
+                <Item marca="Fiat" lancamento={1964}/>
+                <Item marca="Renault" lancamento={1999}/>
+                <Item />
             </ul>
         </>
     )   
