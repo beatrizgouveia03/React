3ebf1a2f8c85569f067e779260391f00be05b45b
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-02-28 10:31:45 -0300

    Aula 9

--- hora-de-codar/src/App.js
+++ hora-de-codar/src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
-import Frase from './components/Frase';
-import List from './components/List';
-import Pessoa from './components/Pessoa';
-import HelloWorld from './components/HelloWorld';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld /> <br />
-      <Pessoa 
-        nome="Rodrigo"
-        idade="28"
-        profissao="Programador"
-        foto='https:/via.placeholder.com/150'
-      />
-      <Frase /> <br />
-      <Frase />
-      <List />
-      
+      <h1>Testando eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
