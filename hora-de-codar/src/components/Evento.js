159fde49ec7053f4b3aa59f94a3e1d25c7fb6b24
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-02-28 10:54:09 -0300

    Aula 11

--- hora-de-codar/src/components/Evento.js
+++ hora-de-codar/src/components/Evento.js
@@ -1,13 +1,26 @@
-function Evento({ numero }) {
+import Button from "./eventos/Button"
+
+function Evento() {
 
     function meuEvento() {
-        console.log(`Opa, fui ativado! ${numero}`)
+        console.log(`Opa, fui ativado!`)
+    }
+
+    function segundoEvento() {
+        console.log('Ativando segundo evento')
     }
 
     return(
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button 
+                event={meuEvento}
+                text="Primeiro evento"
+            />
+            <Button
+                event={segundoEvento}
+                text="Segundo evento"
+            />
         </div>
     )
 }
