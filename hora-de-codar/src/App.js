ba9b0d996c96776adf5bba59bde29ab6a6d02716
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-03-01 10:55:12 -0300

    Aula 14

--- hora-de-codar/src/App.js
+++ hora-de-codar/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import OtherList from './components/secundarios/OtherList';
+import { useState } from 'react';
+import SeuNome from './components/secundarios/SeuNome';
+import Saudacao from './components/secundarios/Saudacao';
+
 
 
 function App() {
 
-  const meusItems = ['React', 'Vue', 'Angular']
+  const [nome, setNome] = useState()
 
   return (
     <div className="App">
-      <h1>Renderização de Listas</h1>
-      <OtherList itens={meusItems}/>
-      <OtherList itens={[]} />
-      
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome}/>     
     </div>
   );
 }
