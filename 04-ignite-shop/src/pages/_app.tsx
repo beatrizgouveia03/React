2d50e903dfc8cf764222c7abb1089869bbca601e
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2023-04-19 11:35:37 -0300

    app styles set

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,7 +1,19 @@
-import React from "react"
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from "../styles/pages/app";
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
-
-export default App
